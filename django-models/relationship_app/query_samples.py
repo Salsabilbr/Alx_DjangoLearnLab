a0e59bad34e99b4cf0568c4968f6ad3b2613a799
Salsabilbr <sabilbr90@gmail.com>
2024-08-17 12:03:02 +0200

    query_samples.py

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,17 @@
+from django.db.models import F
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+author = Author.objects.get(name='John Doe')
+books_by_author = Book.objects.filter(author=author)
+print(books_by_author)
+
+# List all books in a library
+library = Library.objects.get(name='New York Public Library')
+books_in_library = library.books.all()
+print(books_in_library)
+
+# Retrieve the librarian for a library
+library = Library.objects.get(name='New York Public Library')
+librarian = Librarian.objects.get(library=library)
+print(librarian.name)
