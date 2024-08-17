7d22a451b5792a668c6740d58481bff612a58297
Salsabilbr <sabilbr90@gmail.com>
2024-08-17 22:36:09 +0200

    relationship_app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -13,4 +13,12 @@ urlpatterns = [
    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  
    path('logout/', views.logout_view, name='logout'),  
    path('register/', views.register, name='register'),  
-]]               
+]] 
+from django.urls import path
+from. import views
+
+urlpatterns = [
+   path('admin/', views.admin_view, name='admin_view'),
+   path('librarian/', views.librarian_view, name='librarian_view'),
+   path('member/', views.member_view, name='member_view'),
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -32,3 +32,26 @@ def register(request):
 def logout_view(request):  
    auth_views.logout(request)  
    return render(request, 'logout.html')
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+def check_admin(user):
+   return user.userprofile.role == 'Admin'
+
+def check_librarian(user):
+   return user.userprofile.role == 'Librarian'
+
+def check_member(user):
+   return user.userprofile.role == 'Member'
+
+@admin_view = user_passes_test(check_admin)
+def admin_view(request):
+   return render(request, 'admin_view.html')
+
+@user_passes_test(check_librarian)
+def librarian_view(request):
+   return render(request, 'librarian_view.html')
+
+@user_passes_test(check_member)
+def member_view(request):
+   return render(request, 'ember_view.html')
