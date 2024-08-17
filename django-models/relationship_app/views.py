cb911dfac447b6df655e36109d84dcb10fc29096
Salsabilbr <sabilbr90@gmail.com>
2024-08-17 22:53:23 +0200

    views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -35,23 +35,23 @@ def logout_view(request):
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-def check_admin(user):
+def check_Admin(user):
    return user.userprofile.role == 'Admin'
 
-def check_librarian(user):
+def check_Librarian(user):
    return user.userprofile.role == 'Librarian'
 
-def check_member(user):
+def check_Member(user):
    return user.userprofile.role == 'Member'
 
 @admin_view = user_passes_test(check_admin)
-def admin_view(request):
+def Admin_view(request):
    return render(request, 'Admin_view.html')
 
 @user_passes_test(check_librarian)
-def librarian_view(request):
+def Librarian_view(request):
    return render(request, 'Librarian_view.html')
 
 @user_passes_test(check_member)
-def member_view(request):
+def Member_view(request):
    return render(request, 'Member_view.html')
