8d1bc3f3b41c0584fc7781c8e6b7669a4ab750e6
Salsabilbr <sabilbr90@gmail.com>
2024-08-17 22:39:33 +0200

    urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,13 +12,16 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [  
    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  
    path('logout/', views.logout_view, name='logout'),  
-   path('register/', views.register, name='register'),  
+   path('register/', views.register, name='register'),
+   path('Admin/', views.admin_view, name='Admin_view'),
+   path('Librarian/', views.librarian_view, name='Librarian_view'),
+   path('Member/', views.member_view, name='Member_view'),
 ]] 
 from django.urls import path
 from. import views
 
 urlpatterns = [
-   path('admin/', views.admin_view, name='admin_view'),
-   path('librarian/', views.librarian_view, name='librarian_view'),
-   path('member/', views.member_view, name='member_view'),
+   path('Admin/', views.admin_view, name='Admin_view'),
+   path('Librarian/', views.librarian_view, name='Librarian_view'),
+   path('Member/', views.member_view, name='Member_view'),
 ]
