c79f7c21e4521e8958c0c463cbfba49a8f6861c6
Salsabilbr <sabilbr90@gmail.com>
2024-08-25 01:30:26 +0200

    views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+from.models import Book
+
+@permission_required('library.can_view', raise_exception=True)
+def book_list_view(request):
+   books = Book.objects.all()
+   return render(request, 'book_list.html', {'books': books})
+
+@permission_required('library.can_create', raise_exception=True)
+def create_book_view(request):
+   if request.method == 'POST':
+      # Create a new book
+      pass
+   return render(request, 'create_book.html')
+
+@permission_required('library.can_edit', raise_exception=True)
+def edit_book_view(request, pk):
+   book = Book.objects.get(pk=pk)
+   if request.method == 'POST':
+      # Update the book
+      pass
+   return render(request, 'edit_book.html', {'book': book})
+
+@permission_required('library.can_delete', raise_exception=True)
+def delete_book_view(request, pk):
+   book = Book.objects.get(pk=pk)
+   book.delete()
+   return redirect('book_list')
