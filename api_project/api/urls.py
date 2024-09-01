cf18d42c78397b5c508d5b4367b692f1bb91bd56
Salsabilbr <sabilbr90@gmail.com>
2024-09-01 19:36:32 +0200

    api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,3 +4,13 @@ from . import views
 urlpatterns = [  
    path('books/', views.BookList.as_view()),  
 ]
+
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'books', views.BookViewSet)
+
+urlpatterns = [
+   path('', include(router.urls)),
+]
