94a3952523dc0c8909991e70c184bf7552bd8154
Salsabilbr <sabilbr90@gmail.com>
2024-09-14 01:55:52 +0200

    blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,4 +9,6 @@ class Post(models.Model):
    post = models.ForeignKey(Post, on_delete=models.CASCADE)
    created_at = models.DateTimeField(auto_now_add=True)  
    updated_at = models.DateTimeField(auto_now=True)
+
+["Comment(models.Model)"]
 # Create your models here.
