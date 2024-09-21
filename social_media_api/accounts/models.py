1b9937da38122905a81a6aac2a79123f28df9c03
Salsabilbr <sabilbr90@gmail.com>
2024-09-21 22:06:44 +0200

    social_media_api

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models  
+from django.contrib.auth.models import AbstractUser  
+  
+class User(AbstractUser):  
+ bio = models.TextField(blank=True)  
+ profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)  
+ followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+# Create your models here.
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+# Create your views here.
+from rest_framework.response import Response  
+from rest_framework.views import APIView  
+from rest_framework.authtoken.views import ObtainAuthToken  
+from rest_framework.authtoken.models import Token  
+from .serializers import UserSerializer  
+  
+class RegisterView(APIView):  
+   def post(self, request):  
+      serializer = UserSerializer(data=request.data)  
+      if serializer.is_valid():  
+        user = serializer.save()  
+        token, created = Token.objects.get_or_create(user=user)  
+        return Response({'token': token.key})  
+      return Response(serializer.errors, status=400)  
+  
+class LoginView(ObtainAuthToken):  
+   def post(self, request, *args, **kwargs): 
+serializer = self.serializer_class(data=request.data, context={'request': request})  
+      serializer.is_valid(raise_exception=True)  
+      user = serializer.validated_data['user']  
+      token, created = Token.objects.get_or_create(user=user)  
+      return Response({'token': token.key})  
+  
+# accounts/serializers.py  
+from rest_framework import serializers  
+from .models import User  
+  
+class UserSerializer(serializers.ModelSerializer):  
+   class Meta:  
+      model = User  
+      fields = ['username', 'email', 'password', 'bio', 'profile_picture']  
+      extra_kwargs = {'password': {'write_only': True}}  
+  
+   def create(self, validated_data):  
+      user = User.objects.create_user(**validated_data)  
+      return user
+
+class RegisterView(APIView):
+   def post(self, request):
+      ...
+      return Response({'token': token.key})
+
+class LoginView(ObtainAuthToken):
+   def post(self, request, *args, **kwargs):
+      ...
+      return Response({'token': token.key})
