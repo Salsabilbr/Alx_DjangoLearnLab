87b8d9529e3d05da0406296a4fd71c6540fc4c7e
Salsabilbr <sabilbr90@gmail.com>
2024-09-21 22:19:32 +0200

    serializers.py

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework.authtoken.models import Token 
+
+class CommentSerializer(serializers.serializer):
+    ["serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user"]
+
