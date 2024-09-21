3969cbfe4412372606e6e911c6833fad7668c5ca
Salsabilbr <sabilbr90@gmail.com>
2024-09-21 22:48:58 +0200

    settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  
-    'accounts',
+    'accounts',
     'rest_framework.authtoken',
 ]
 
