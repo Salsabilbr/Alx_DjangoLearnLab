1307c188245349ab0e10bc03e4b2dac6b711e3ff
Salsabilbr <sabilbr90@gmail.com>
2024-09-21 22:33:47 +0200

    accounts

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,20 +2,3 @@ from rest_framework.authtoken.models import Token
 
 class CommentSerializer(serializers.serializer):
     ["serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user"]
-from rest_framework import serializers  
-from .models import Post, Comment  
-  
-class PostSerializer(serializers.ModelSerializer):  
-   author = serializers.StringRelatedField(read_only=True)  
-  
-   class Meta:  
-      model = Post  
-      fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at']  
-  
-class CommentSerializer(serializers.ModelSerializer):  
-   author = serializers.StringRelatedField(read_only=True)  
-   post = serializers.PrimaryKeyRelatedField(read_only=True)  
-  
-   class Meta:  
-      model = Comment  
-      fields = ['id', 'content', 'author', 'post', 'created_at', 'updated_at']
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -46,30 +46,3 @@ class LoginView(ObtainAuthToken):
    def post(self, request, *args, **kwargs):
       ...
       return Response({'token': token.key})
-
-from rest_framework import viewsets  
-from rest_framework.permissions import IsAuthenticated  
-from .models import Post, Comment  
-from .serializers import PostSerializer, CommentSerializer  
-  
-class PostViewSet(viewsets.ModelViewSet):  
-   queryset = Post.objects.all()  
-   serializer_class = PostSerializer  
-   permission_classes = [IsAuthenticated]  
-  
-   def perform_create(self, serializer):  
-      serializer.save(author=self.request.user)  
-  
-   def perform_update(self, serializer):  
-      serializer.save(author=self.request.user)  
-  
-class CommentViewSet(viewsets.ModelViewSet):  
-   queryset = Comment.objects.all()  
-   serializer_class = CommentSerializer  
-   permission_classes = [IsAuthenticated]  
-  
-   def perform_create(self, serializer):  
-      serializer.save(author=self.request.user)  
-  
-   def perform_update(self, serializer):  
-      serializer.save(author=self.request.user)
